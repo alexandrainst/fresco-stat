08a15a9ae2e2a4f298d5f76425b859486e70e907
Jonas Lindstrøm <jonas.lindstrom@alexandra.dk>
2020-09-18 11:53:49 +0200

    Updata javedoc

--- src/main/java/dk/alexandra/fresco/stat/survival/SurvivalInfoDiscrete.java
+++ src/main/java/dk/alexandra/fresco/stat/survival/SurvivalInfoDiscrete.java
@@ -8,7 +8,8 @@ import java.util.List;
 /**
  * Represents a data point in data for survival analysis with only discrete covariates on finite
  * sets. Each entry in the covariates set is an 0-1 indicator vector with exactly one non-zero value
- * indicating the value of that particular covariate.
+ * indicating the value of that particular covariate along with a value representing the value as an
+ * integer. E.g. ((0,1,0,0), 1) or ((0,0,1,0), 2) if the covariate has 4 different values.
  */
 public class SurvivalInfoDiscrete {
 
